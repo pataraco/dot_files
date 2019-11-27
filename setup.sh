df2af03b86f5e7bfd051c7f25449f86cf0aa9fd4
Patrick Raco <pataraco@gmail.com>
2019-11-27 01:08:42 -0800

    improve the setup script a little

--- setup.sh
+++ setup.sh
@@ -10,24 +10,40 @@
 #        install homebrew
 #        $ brew install git
 
-SRC_REPO="$HOME/repos/dot_files"
+# set up global variables
+SRC_REPO="$HOME/repos/pataraco/dot_files"
 ORIG_DIR="$HOME/.orig"
+
+# create a directory for original files if it doesn't exist
 [ ! -d $ORIG_DIR ] && { echo -en "creating dir ($ORIG_DIR)... "; mkdir $ORIG_DIR; echo "done"; }
+
+# change working directory to the repo root
 cd $SRC_REPO
+
+# get list of files to process and create symlinks
 for file in $(ls -1d .[a-z]* | grep -wv .git); do
-   echo "processing file: $file"
-   if [ -f $HOME/$file ]; then
-      echo -en "   moving original file ($HOME/$file) to $ORIG_DIR... "
-      mv $HOME/$file $ORIG_DIR
-      echo "done"
+   echo -n "processing: $file... "
+   if [ -f $file ]; then  # source file is a regular file, create link
+      if [ -L $HOME/$file ]; then  # existing symlink
+         echo -n "removing existing symlink... "
+         rm -f $HOME/$file
+      elif [ -f $HOME/$file ]; then  # existing file
+         echo -n "saving original file... "
+         mv $HOME/$file $ORIG_DIR
+         saved="True"
+      else  # not found
+         echo -n "existing file/symlink not found..."
+      fi
+      echo -n "creating symlink... "
+      ln -s $SRC_REPO/$file $HOME/$file
+   else  # source file is not a regular file, don't link
+      echo -n "not a regular file, not creating symlink... "
    fi
-   if [ -h $HOME/$file ]; then
-      echo -en "   removing old symlink ($HOME/$file)... "
-      rm -f $HOME/$file
-      echo "done"
-   fi
-   echo -en "   creating symlink: $HOME/$file -> $SRC_REPO/$file... "
-   ln -s $SRC_REPO/$file $HOME/$file
    echo "done"
 done
+
+# notify user of any saved files and their location
+[ "$saved" == "True" ] && echo "original files saved to ${ORIG_DIR/$HOME/\$HOME}"
+
+# restore current working directory location
 cd -
