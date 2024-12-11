0ba224504c87e6d0d17872bb749fb8dbb22519c7
Patrick Raco <patrick.raco@autodesk.com>
2024-12-11 11:34:46 -0800

    update neovim setup

--- setup.sh
+++ setup.sh
@@ -26,27 +26,26 @@ DESIRED_SHELL="/bin/bash"
 
 # make sure running on MacBook
 if [ "$(uname)" != "Darwin" ]; then
-   echo "Kindof only supported on MacBooks"
-   exit
+  echo "Kindof only supported on MacBooks"
+  exit
 fi
 
 # set date/time format in menu bar
 # EEE - day of the week, MMM - month, d - date
 # other fomats: "EEE MMM d h:mm:ss a" e.g.: "Sat Jun 3 5:03:23 pm"
-defaults write com.apple.menuextra.clock DateFormat -string "EEE MMM d HH:mm" \
-   && killall SystemUIServer
+defaults write com.apple.menuextra.clock DateFormat -string "EEE MMM d HH:mm" &&
+  killall SystemUIServer
 
 # set login shell to /bin/bash
 [ "$SHELL" != "$DESIRED_SHELL" ] && chsh -s "$DESIRED_SHELL"
 
 # create a directory for original files if it doesn't exist
-[ ! -d "$ORIG_DIR" ] \
-   && {
-      echo -en "creating dir ($ORIG_DIR)... "
-      mkdir "$ORIG_DIR"
-      echo "done"
-   }
-
+[ ! -d "$ORIG_DIR" ] &&
+  {
+    echo -en "creating dir ($ORIG_DIR)... "
+    mkdir "$ORIG_DIR"
+    echo "done"
+  }
 
 # change working directory to the repo root
 OWD=$(pwd)
@@ -54,65 +53,61 @@ cd "$SRC_REPO" || exit
 
 # install Homebrew
 if brew --version; then
-   echo "Homebrew already installed"
+  echo "Homebrew already installed"
 else
-   echo "Installing Homebrew"
-   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)" || exit
+  echo "Installing Homebrew"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)" || exit
 fi
 
 # install packages specified in Brewfile
 # created with `brew bundle dump`
 if brew bundle check; then
-   echo "Homebrew: Don't need to install any additonal packages"
+  echo "Homebrew: Don't need to install any additonal packages"
 else
-   echo "Homebrew: Installing packages specified in Brewfile"
-   brew bundle
+  echo "Homebrew: Installing packages specified in Brewfile"
+  brew bundle
 fi
 
 # get list of files to process and create symlinks
 # shellcheck disable=SC2010
 for file in $(ls -1d .[a-z]* | grep -wv .git); do
-   echo -n "processing: $file... "
-   if [ -f "$file" ]; then  # source file is a regular file, create link
-      if [ -L "$HOME/$file" ]; then  # existing symlink
-         echo -n "removing existing symlink... "
-         rm -f "$HOME/$file"
-      elif [ -f "$HOME/$file" ]; then  # existing file
-         echo -n "saving original file... "
-         mv "$HOME/$file" "$ORIG_DIR"
-         files_saved=true
-      else  # not found
-         echo -n "existing file/symlink not found..."
-      fi
-      echo -n "creating symlink... "
-      ln -s "$SRC_REPO/$file" "$HOME/$file"
-   else  # source file is not a regular file, don't link
-      echo -n "not a regular file, not creating symlink... "
-   fi
-   echo "done"
+  echo -n "processing: $file... "
+  if [ -f "$file" ]; then         # source file is a regular file, create link
+    if [ -L "$HOME/$file" ]; then # existing symlink
+      echo -n "removing existing symlink... "
+      rm -f "$HOME/$file"
+    elif [ -f "$HOME/$file" ]; then # existing file
+      echo -n "saving original file... "
+      mv "$HOME/$file" "$ORIG_DIR"
+      files_saved=true
+    else # not found
+      echo -n "existing file/symlink not found..."
+    fi
+    echo -n "creating symlink... "
+    ln -s "$SRC_REPO/$file" "$HOME/$file"
+  else # source file is not a regular file, don't link
+    echo -n "not a regular file, not creating symlink... "
+  fi
+  echo "done"
 done
 
 # notify user of any saved files and their location
 $files_saved && echo "original dot files saved to ${ORIG_DIR/$HOME/\$HOME}"
 
-# set up & configure 'neovim' (with .vimrc)
-mkdir -p "$HOME/.config/nvim"
-ln -s "$HOME/.vimrc" "$HOME/.config/nvim/init.vim"
-curl -fLo "$HOME/.local/share/nvim/site/autoload/plug.vim" --create-dirs \
-   https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-vim +'PlugInstall --sync' +qa
-wget https://raw.githubusercontent.com/znake/znake-vim/master/colors/znake.vim \
-   -P "$HOME/.config/nvim/colors"
-
-# install & setup 'tmux'
-mkdir -p "$HOME/repos/tmux"
-cd "$HOME/repos/tmux" || exit
-git clone https://github.com/tmux/tmux.git
-cd tmux || exit
-sh autogen.sh
-./configure && make
-sudo cp tmux /usr/local/bin
-git clone https://github.com/tmux-plugins/tpm "$HOME/.tmux/plugins/tpm"
+# set up & configure 'neovim' (with Lua and LazyVim)
+NEOVIM_CONFIG="$HOME/.config/nvim"
+LAZYVIM_URL="https://github.com/LazyVim/starter"
+LUA_SRC="$SRC_REPO/.config/nvim/lua"
+echo "Setting up Neovim"
+if [[ -d "$NEOVIM_CONFIG" ]]; then
+  echo "Neovim config already exists at '$NEOVIM_CONFIG' - saving original as '$NEOVIM_CONFIG.orig'"
+  mv "$NEOVIM_CONFIG" "$NEOVIM_CONFIG.orig"
+fi
+echo "Cloning LazyVim (Starter) [$LAZYVIM_URL] into Neovim config directory [$NEOVIM_CONFIG]"
+git clone "$LAZYVIM_URL" "$NEOVIM_CONFIG"
+rm -rf "$NEOVIM_CONFIG/.git"
+echo "Copying Lua configs from '$LUA_SRC' into '$NEOVIM_CONFIG'"
+cp -Rv "$LUA_SRC" "$NEOVIM_CONFIG"
 
 # restore current working directory location
 cd "$OWD" || exit
